ae2323ed764f3fa9f08bb50b3f09cc7dc0e2183b
elenammorgan <elena.morgan1@marist.edu>
2024-10-30 20:25:23 -0400

    done

--- exercise2.js
+++ exercise2.js
@@ -1,3 +1,3 @@
 setTimeout(() => {
     console.log('This message is displayed after 3 seconds.');
-    }, 3000); // this file takes a few seconds to run, then it displays the message above in the debug console hel
+    }, 3000); // this file takes a few seconds to run, then it displays the message above in the debug console
